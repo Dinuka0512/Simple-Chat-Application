0b9dae3b67ab1f447b98003c1f05022ec682cfee
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-04-09 11:07:46 +0530

    Created the Chat application to cahat while say exit

--- src/main/java/com/example/chatapplication/controller/ClientConto.java
+++ src/main/java/com/example/chatapplication/controller/ClientConto.java
@@ -2,14 +2,52 @@ package com.example.chatapplication.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class ClientConto {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientConto implements Initializable {
     @FXML
     private TextField txtMessage;
 
     @FXML
-    void sentMessageToServer(ActionEvent event) {
+    private TextArea txtArea;
+
+    private Socket socket;
+    private DataInputStream dataInputStream;
+    private String message = "";
+    private DataOutputStream dataOutputStream;
+
+    @FXML
+    void sentMessageToServer(ActionEvent event) throws IOException {
+        dataOutputStream = new DataOutputStream( socket.getOutputStream());
+        dataOutputStream.writeUTF(txtMessage.getText());
+        dataOutputStream.flush();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        new Thread(() -> {
+            try {
+                socket = new Socket("localhost",4000);
+                txtArea.appendText("Connected to the server \n");
+
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                while (!message.equals("exit")){
+                    message=dataInputStream.readUTF();
+                    txtArea.appendText(message+"\n");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
 
     }
 }
--- src/main/java/com/example/chatapplication/controller/ServerContro.java
+++ src/main/java/com/example/chatapplication/controller/ServerContro.java
@@ -1,4 +1,63 @@
 package com.example.chatapplication.controller;
 
-public class ServerContro {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ServerContro  implements Initializable {
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+    private String message = "";
+
+    @FXML
+    private TextField txtMessage;
+
+    @FXML
+    private TextArea txtArea;
+
+    @FXML
+    void sentMessageToClient(ActionEvent event) throws IOException {
+        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        dataOutputStream.writeUTF(txtMessage.getText());
+        dataOutputStream.flush();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        new Thread(()->{
+            try {
+                serverSocket = new ServerSocket(4000);
+                txtArea.appendText("Server Started...\n");
+
+                //Client Side Request Accept
+                // Add message to Local host
+                socket = serverSocket.accept();
+                txtArea.appendText("Client Connected!\n");
+
+
+                //Ena massage ekak Read Kara ganna Data Input Stream use kkaranawa
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                while (!message.equals("exit")){
+                    message=dataInputStream.readUTF();
+                    txtArea.appendText(message+"\n");
+                }
+                //message
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+
+    }
 }
